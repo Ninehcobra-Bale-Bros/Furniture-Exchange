fc6a0ecba94df9e58f5f7938d97f19eecf1a856b
bachlex1608 <lxbachit03.working@gmail.com>
2024-10-29 19:59:04 +0700

    fix socket

--- client/admin-frontend/src/environments/environment.ts
+++ client/admin-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // API_URL: 'http://13.250.4.21/api/v1',
-  SOCKET_URL: 'http://13.250.4.21:3000',
+  // SOCKET_URL: 'http://13.250.4.21:3000',
   API_URL: '/api/v1',
-  // SOCKET_URL: 'http://34.143.175.164:3002/',
+  SOCKET_URL: '/',
 };
