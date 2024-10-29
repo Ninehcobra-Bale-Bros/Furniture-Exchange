df66872d657bacf0c8dcae48ab308c4ffc59cfac
bachlex1608 <lxbachit03.working@gmail.com>
2024-10-29 15:04:58 +0700

    wip

--- client/admin-frontend/src/environments/environment.ts
+++ client/admin-frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   // API_URL: 'http://13.250.4.21/api/v1',
   // SOCKET_URL: 'http://13.250.4.21:3002/',
   API_URL: '/api/v1',
-  SOCKET_URL: '/:3002/',
+  SOCKET_URL: 'http://34.143.175.164:3002/',
 };
